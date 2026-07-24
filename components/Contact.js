aaaea7a9492c921b0241cea6ece3c7184769def8
Ghazi Abdul Mateen Hussain <ghaziabdulmateen0786@gmail.com>
2026-07-24 14:56:41 +0500

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -62,7 +62,7 @@ export default function Contact() {
         .join("&");
 
     try {
-      const res = await fetch("/", {
+      const res = await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encode({
